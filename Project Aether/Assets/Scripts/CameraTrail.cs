cf5961c470d1344de554ed3f33d911aacc86f7ec
Naman <sumannaman7@gmail.com>
2021-05-10 13:47:45 +0530

    git s

--- Project Aether/Assets/Scripts/CameraTrail.cs
+++ Project Aether/Assets/Scripts/CameraTrail.cs
@@ -8,8 +8,6 @@ public class CameraTrail : MonoBehaviour
     public float trailDistance =5.0f;
     public float heightOffset=3.0f;
   
-    // Start is called before the first frame update
-      // Update is called once per frame
     void Update()
     {
         Vector3 followPos = target.position -target.forward*trailDistance;
